cbf9fd390504a9b8cad8d51e2ccd46d0caa3a2e7
Yaohong Xiang <xiang.yao@northeastern.edu>
2023-04-12 16:40:26 -0400

    1

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,18 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+
+    //add custom method
+
+    // add sort by lastname
+    public List<Employee> findAllByOrderByLastNameAsc();
+	
+}
